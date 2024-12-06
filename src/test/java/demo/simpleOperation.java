819882042909d2bec849ba8affe15f9f18d3afd3
DELL <DELL@DESKTOP-L8HLN3U>
2024-12-06 16:43:59 +0530

    Simple operation committed

---
+++ src/test/java/demo/simpleOperation.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class simpleOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi hello");
+		int a=10, b=20;
+		int c=a+b;
+		System.out.println("addition of a & b is"  +c);
+	}
+
+}
